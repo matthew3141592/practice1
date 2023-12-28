4ca45e4deb2045a4f00f58091dcfdb7bfd9dbc7e
matthew3141592 <154984434+matthew3141592@users.noreply.github.com>
2023-12-28 09:09:57 +0100

    Add files via upload

---
+++ practice1.js
@@ -0,0 +1,8 @@
+function navBarToggle() {
+    document.getElementById('navBar').style.visibility = 'visible'
+    document.getElementById('navBar').style.width = '70%'
+    document.getElementById('navHome').style.width = '50%'
+    document.getElementById('navContact').style.width = '50%'
+    document.getElementById('navJoin').style.width = '50%'
+    document.getElementById('logo').style.paddingRight = '0'
+}
